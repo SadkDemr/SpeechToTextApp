43f01539f53925f9e1afb18936a84a1f864772c8
SadkDemr <65198970+SadkDemr@users.noreply.github.com>
2022-02-19 01:33:47 +0300

    SpeechToTextApp
    Speech To Text App

---
+++ SpeechToTextApp/blog.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect
+import os
+import speech_recognition as sr
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('index.html', transcript=transcript)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+    
+if __name__ == "_main_":
+    app.run(debug=True, threaded=True)
